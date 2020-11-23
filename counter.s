aeef028bee3e838142a2d7601d2b22cf50df9dd1
john <john@johnvanweel.nl>
2020-11-23 16:17:50 +0100

    Implemented number on display

---
+++ counter.s
@@ -0,0 +1,176 @@
+
+PORTB = $6000
+PORTA = $6001
+DDRB = $6002
+DDRA = $6003
+
+value = $0200   ; 2 bytes
+mod10 = $0202   ; 2 bytes
+message = $0204 ; 6 bytes
+
+E =  %10000000
+RW = %01000000
+RS = %00100000
+
+ .org $8000
+
+reset:
+ ldx #$ff
+ txs
+
+ lda #%11111111 ; Set all pins on port B to output
+ sta DDRB
+ lda #%11100000 ; Set 3 pins on port A to output
+ sta DDRA
+
+ lda #%00111000 ; Set 8 bit mode;  2-line display; 5x8 font
+ jsr lcd_instruction
+ lda #%00001110 ; Display on; cursor on; blink off
+ jsr lcd_instruction
+ lda #%00000110 ; Increment and shift cursor
+ jsr lcd_instruction
+ lda #%00000001 ; Clear screen
+ jsr lcd_instruction
+
+; Initialize message to 0
+ lda #0
+ sta message
+
+; Initialize value to be the number we want to convert
+ lda number         ; Load first byte of number into RAM -> value
+ sta value
+ lda number + 1     ; Load second byte of number into RAM -> value + 1
+ sta value+1
+
+divide:
+; Initialize remainder to 0
+ lda #0             ; Same with mod10
+ sta mod10
+ sta mod10 + 1
+ clc                ; Clear carry bit
+
+ ldx #16
+divloop:
+; Rotate quotient and remainder
+ rol value
+ rol value + 1
+ rol mod10
+ rol mod10 + 1
+
+; a,y = dividend - divisor
+ sec
+ lda mod10
+ sbc #10
+ tay                ; Save low byte in Y
+ lda mod10 + 1
+ sbc #0
+ bcc ignore_result  ; branch if dividend < divisor
+ sty mod10
+ sta mod10 + 1
+
+ignore_result:
+ dex                ; Decrement X (starting at 16)
+ bne divloop        ; While x != 0
+ rol value          ; Shift in the last bit of the quotient
+ rol value + 1
+
+ lda mod10
+ clc
+ adc #"0"
+ jsr push_char
+
+; If value < 0, then continue dividing
+ lda value          ; Load value
+ ora value + 1      ; OR it with value + 1
+ bne divide         ; If not all bits are 0, repeat divide
+
+ ldx #0
+print:
+ lda message,x
+ beq loop
+ jsr print_char
+ inx
+ jmp print
+
+loop:
+ jmp loop
+
+number: .word 1729
+
+; Add the character in A-reg to beginning of null-terminated string 'message'
+push_char:
+ pha        ; Push first char on stack
+ ldy #0
+
+char_loop:
+ lda message,y ; Get char on string and push into X
+ tax
+ pla
+ sta message,y ; Pull char off stack and add it to the string
+ iny
+ txa
+ pha            ; Push char from string onto stack
+ bne char_loop
+
+ pla
+ sta message,y  ; Pull the null off the stack and add to the end of message
+
+ rts
+
+
+lcd_wait:
+ pha            ; Store A-reg to stack
+ lda #%00000000 ; Input
+ sta DDRB       ; Set DDR on PORTB as Input
+
+lcd_busy:
+ lda #RW
+ sta PORTA
+ lda #(RW | E)
+ sta PORTA
+ lda PORTB      ; Load R/W from LCD
+ and #%10000000
+ bne lcd_busy
+  
+ lda #RW
+ sta PORTA
+ 
+ lda #%11111111 ; Output
+ sta DDRB       ; Set DDR on PortB as Output
+
+ pla            ; Pop A-reg from stack
+ rts
+
+
+lcd_instruction:
+ jsr lcd_wait
+ sta PORTB
+
+ lda #0         ; Clear RS/RW/E bits
+ sta PORTA
+
+ lda #E         ; Set E bit to send instruction
+ sta PORTA
+
+ lda #0         ; Clear RS/RW/E bits
+ sta PORTA
+ rts
+
+print_char:
+ jsr lcd_wait
+ sta PORTB
+
+ lda #RS         ; Clear RS/RW/E bits
+ sta PORTA
+
+ lda #(RS | E)   ; Set E bit to send instruction
+ sta PORTA
+
+ lda #RS         ; Clear E bits
+ sta PORTA
+
+ rts
+ 
+ .org $fffc
+ .word reset
+ .word $0000 
